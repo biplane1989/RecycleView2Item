05da88853fbb9b4e33fee71b65728ceac10e85a0
giangtd <68835891+tomatoesamerica@users.noreply.github.com>
2021-03-05 09:24:53 +0700

    sss

--- app/src/main/java/com/example/recycleview2item/HomeViewmodel.kt
+++ app/src/main/java/com/example/recycleview2item/HomeViewmodel.kt
@@ -6,13 +6,15 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewmodel : ViewModel() {
 
+    var status: Boolean = false
     private val _listData = MutableLiveData<List<DataItem>>()
     val listData: LiveData<List<DataItem>> get() = _listData
     var lists = ArrayList<DataItem>()
 
     init {
         val datas = ArrayList<DataItem>()
-
+        datas.add(DataItem(6, "orange", false, "C ", 0))
+        datas.add(DataItem(9, "orange", false, "D ", 0))
         datas.add(DataItem(1, "orange", false, "A ", 0))
         datas.add(DataItem(2, "orange", false, "A ", 0))
         datas.add(DataItem(3, "orange", false, "B ", 0))
@@ -23,20 +25,23 @@ class HomeViewmodel : ViewModel() {
         datas.add(DataItem(6, "orange", false, "C ", 0))
         datas.add(DataItem(7, "orange", false, "D ", 0))
         datas.add(DataItem(8, "orange", false, "D ", 0))
-        datas.add(DataItem(9, "orange", false, "D ", 0))
+
 
         _listData.value = datas
         lists = datas
     }
 
     fun changeData() {
-        val listData = ArrayList(_listData.value)
+        var list : List<DataItem> = lists
         val newList = ArrayList<DataItem>()
-//        listData = listData.sortedBy { it.titleHeader }
+
+        list = list.sortedBy { it.titleHeader }
+
+        newList.add(DataItem(1,"",true,"All", list.size))
         var hearder = ""
         var count = 1
-        var index = 0
-        for (item in listData) {
+        var index = 1
+        for (item in list) {
             if (!item.titleHeader.equals(hearder)) {
 
                 hearder = item.titleHeader
@@ -54,27 +59,32 @@ class HomeViewmodel : ViewModel() {
             } else {
                 count++
             }
-            if (item == listData.get(listData.lastIndex)){
+            if (item == list.get(list.lastIndex)){
                 newList[index - 1].count = count
             }
-
         }
 
         _listData.value = newList
+
     }
 
     fun filterTypeFolder(dataItem: DataItem){
 
-        var newList = ArrayList<DataItem>(lists)
-        val listData = ArrayList<DataItem>()
+        if (dataItem.titleHeader.equals("All")){
+            _listData.value = lists
+        }else{
+            var newList = ArrayList<DataItem>(lists)
+            val listData = ArrayList<DataItem>()
 
-        for (item in newList){
-            if (item.titleHeader.equals(dataItem.titleHeader)){
-                listData.add(item)
+            for (item in newList){
+                if (item.titleHeader.equals(dataItem.titleHeader)){
+                    listData.add(item)
+                }
             }
-        }
+            _listData.value= listData
 
-        _listData.value= listData
+            status = true
+        }
     }
 
 }
